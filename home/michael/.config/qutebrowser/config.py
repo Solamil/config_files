747095a5670ce659ecf4af75506d7b209d2a446a
Michal Kukla <michal.kukla@tul.cz>
2020-07-03 17:57:47 +0200

    qutebrowser, fonts, lightdm

---
+++ home/michael/.config/qutebrowser/config.py
@@ -0,0 +1,13 @@
+c.url.searchengines = {'DEFAULT': 'https://search.snopyta.org/?q={}',
+                       'yt':'http://www.youtube.com/results?search_query={}'}
+c.url.default_page = 'https://search.snopyta.org'
+c.url.start_pages = c.url.default_page
+c.content.geolocation = False 
+c.content.notifications = False
+c.downloads.location.directory = "/home/michael/downloads/"
+c.fonts.default_size = "13pt"
+c.scrolling.smooth = True
+#c.messages.timeout
+# c.editor.command = ["nvim"]
+# config.unbind('<Ctrl-v>', mode='normal')
+# config.bind('<Ctrl-v>', 'spawn mpv {url}')
